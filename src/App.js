6e276181a86ff635afa1399795324dc18313c3d7
eunsxx <eunseo120@naver.com>
2021-12-26 23:30:27 +0900

    p.186 ~ p.215

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
-import React, {Component} from "react";
-import LifeCycleSample from "./LifeCycleSample";
+import React, {useState} from "react";
+import Average from "./Average";
 
-// 랜덤 색상을 생성
-function getRandomColor() {
-    return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
-
-class App extends Component {
-    state = {
-        color: '#000000'
-    }
-    handleClick = () => {
-        this.setState({
-            color: getRandomColor()
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <button onClick={this.handleClick}>랜덤 색상</button>
-                <LifeCycleSample color={this.state.color}/>
-            </div>
-        );
-    }
-}
+const App = () => {
+    return <Average />;
+};
 
 export default App;
